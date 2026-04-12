8bea93d69805d1b860863465d7d1be3636fbf8be
BigBoyDejv <jan.rusin3038@gmail.com>
2026-04-12 13:09:29 +0200

    fix syntax error in admin

--- src/pages/Admin.jsx
+++ src/pages/Admin.jsx
@@ -5,10 +5,10 @@ import {
   LogOut, LayoutDashboard, Settings, Search, Filter, 
   ChevronRight, AlertCircle, CheckCircle2, X
 } from 'lucide-react'
-import { clsx, type ClassValue } from 'clsx'
+import { clsx } from 'clsx'
 import { twMerge } from 'tailwind-merge'
 
-function cn(...inputs: ClassValue[]) {
+function cn(...inputs) {
   return twMerge(clsx(inputs))
 }
 
